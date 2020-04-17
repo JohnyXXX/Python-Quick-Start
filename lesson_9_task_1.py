795d9c73863a8335670302ae56724dddf319ab58
JohnyXXX <johnyeev@gmail.com>
2020-04-17 16:46:23 +0500

    Урок 9 Задание 1

--- lesson_9_task_1.py
+++ lesson_9_task_1.py
@@ -1,5 +1,18 @@
+"""
+Написать программу, которая на
+любой вопрос отвечает "да" или "нет"
+"""
 import random
 
-while True:
+
+def answer():
+    x = random.choice([0, 1])
+    if x == 0:
+        return 'Нет'
+    else:
+        return 'Да'
+
+
+for i in range(10):
     question = input('Введите вопрос: ')
-    print(f'{question} ', random.choice(['Да', 'Нет']))
+    print('Ответ:', answer())
