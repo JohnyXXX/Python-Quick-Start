536d5fde99b332d74226f9bbaf23e1dad6b5dc30
JohnyXXX <johnyeev@gmail.com>
2020-04-22 16:31:04 +0500

    Урок 12 Задание 1 Исправление1

--- lesson_12_task_1.py
+++ lesson_12_task_1.py
@@ -5,15 +5,18 @@
 
 
 class Human:
-    def __init__(self, fullname, birth_date, sex, height, weight):
+    def __init__(self, fullname, birth_date, sex, height):
         self.fullname = fullname
         self.birth_date = birth_date
         self.sex = sex
         self.height = height
+
+
+class Weight(Human):
+    def __init__(self, fullname, birth_date, sex, height, weight):
+        Human.__init__(self, fullname, birth_date, sex, height)
         self.weight = weight
 
-
-class Nutrition(Human):
     def eat(self, add_weight):
         self.weight += add_weight
 
