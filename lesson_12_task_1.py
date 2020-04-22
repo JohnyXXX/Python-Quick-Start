71434ee8f9b5f0089ae74c6ed6d9ed78fcf9045f
JohnyXXX <johnyeev@gmail.com>
2020-04-22 16:59:00 +0500

    Урок 12 Задание 1 Исправление2

--- lesson_12_task_1.py
+++ lesson_12_task_1.py
@@ -4,17 +4,8 @@
 """
 
 
-class Human:
-    def __init__(self, fullname, birth_date, sex, height):
-        self.fullname = fullname
-        self.birth_date = birth_date
-        self.sex = sex
-        self.height = height
-
-
-class Weight(Human):
-    def __init__(self, fullname, birth_date, sex, height, weight):
-        Human.__init__(self, fullname, birth_date, sex, height)
+class Weight:
+    def __init__(self, weight):
         self.weight = weight
 
     def eat(self, add_weight):
@@ -22,3 +13,17 @@ class Weight(Human):
 
     def starve(self, reduce_weight):
         self.weight -= reduce_weight
+
+
+class Human(Weight):
+    def __init__(self, fullname, birth_date, sex, height, weight):
+        self.fullname = fullname
+        self.birth_date = birth_date
+        self.sex = sex
+        self.height = height
+        super().__init__(weight)
+
+    def __str__(self):
+        return f'Person({self.fullname}, {self.birth_date}, {self.sex}, {self.height}, {self.weight})'
+
+# ivan = Human('Иванов Иван Иванович', '01.01.1999', 'мужской', 167, 74)
